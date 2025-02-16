0dad157125508c91c6d3cc36aa256693038657c6
Mokhoulani <mohammad.ko555@gmail.com>
2025-02-16 18:41:15 +0100

    in this way to implement api test

--- Server/Tests/Api.Tests/Controllers/UserControllerTests.cs
+++ Server/Tests/Api.Tests/Controllers/UserControllerTests.cs
@@ -1,21 +1,25 @@
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Xunit;
 using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
 
-public class UserControllerTests : IClassFixture<WebApplicationFactory<Program>>
+namespace Api.Tests.Controllers;
+
+public class UserControllerTests(WebApplicationFactory<Program> factory)
+    : IClassFixture<WebApplicationFactory<Program>>
 {
-    private readonly HttpClient _client;
-
-    public UserControllerTests(WebApplicationFactory<Program> factory)
-    {
-        _client = factory.CreateClient();
-    }
+    private readonly HttpClient _client = factory.CreateClient();
 
     [Fact]
     public async Task GetUsers_Should_Return_OK()
     {
+        // Arrange
+        var userId = "6D8116CD-96C2-4DCB-B67B-A6122E1FA230";
+
         // Act
-        var response = await _client.GetAsync("/api/user/id/6D8116CD-96C2-4DCB-B67B-A6122E1FA230");
+        var response = await _client.GetAsync($"/api/user/{userId}");
+
+        // Log the response content for debugging
+        var content = await response.Content.ReadAsStringAsync();
 
         // Assert
         response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
