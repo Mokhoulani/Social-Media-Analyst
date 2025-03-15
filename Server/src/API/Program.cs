00ca04664453075b860f1635388707cfcf21dec3
Mokhoulani <mohammad.ko555@gmail.com>
2025-03-15 22:44:53 +0100

    fix test issue

--- Server/src/API/Program.cs
+++ Server/src/API/Program.cs
@@ -6,99 +6,60 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Quartz;
 using Serilog;
 
-public partial class Program
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
+
+builder.Services.AddControllers();
+
+builder.Services.AddInfrastructureLayer(builder.Configuration);
+builder.Services.AddApplicationLayer(builder.Environment);
+
+builder.Services.AddQuartzHostedService();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.ConfigureOptions<JwtOptionsSetup>();
+builder.Services.ConfigureOptions<RefreshTokenOptionsSetup>();
+builder.Services.ConfigureOptions<JwtBearerOptionsSetup>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer();
+
+builder.Services.AddOpenApi();
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+
+
+app.UseDefaultFiles();
+app.MapStaticAssets();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public static void Main(string[] args)
-    {
-        ConfigureLogger();
-
-        try
-        {
-            StartApplication(args);
-        }
-        catch (Exception e)
-        {
-            Log.Fatal(e, "Error during startup");
-        }
-        finally
-        {
-            Log.Information("Shutting down");
-            Log.CloseAndFlush();
-        }
-    }
-
-    private static void ConfigureLogger()
-    {
-        Log.Logger = new LoggerConfiguration()
-            .WriteTo.Console()
-            .CreateBootstrapLogger();
-    }
-
-    private static void StartApplication(string[] args)
-    {
-        Log.Information("Starting up");
-
-        var builder = WebApplication.CreateBuilder(args);
-        ConfigureServices(builder);
-        Configure(builder.Build()).Run();
-    }
-
-    private static void ConfigureServices(WebApplicationBuilder builder)
-    {
-        builder.Host.UseSerilog((ctx, lc) => lc.ReadFrom.Configuration(ctx.Configuration));
-        builder.Services.AddControllers();
-
-        builder.Services.AddInfrastructureLayer(builder.Configuration);
-        builder.Services.AddApplicationLayer(builder.Environment);
-
-        builder.Services.AddQuartzHostedService();
-
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
-        builder.Services.ConfigureOptions<JwtOptionsSetup>();
-        builder.Services.ConfigureOptions<RefreshTokenOptionsSetup>();
-        builder.Services.ConfigureOptions<JwtBearerOptionsSetup>();
-
-        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer();
-
-        builder.Services.AddOpenApi();
-        builder.Services.AddReverseProxy()
-            .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
-        builder.Services.AddHealthChecks();
-    }
-
-    private static WebApplication Configure(WebApplication app)
-    {
-        
-        app.UseDefaultFiles();
-        app.MapStaticAssets();
-
-
-        if (app.Environment.IsDevelopment())
-        {
-            app.MapOpenApi();
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.MapReverseProxy();
-        app.UseSerilogRequestLogging();
-        app.UseProblemDetails();
-        app.UseHttpsRedirection();
-        app.UseRouting();
-        app.UseAuthentication();
-        app.UseAuthorization();
-
-        app.UseHealthChecks("/health");
-
-        app.MapControllers();
-        app.MapFallbackToFile("/index.html");
-        app.Run();
-        return app;
-    }
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+
+app.UseSerilogRequestLogging();
+app.UseProblemDetails();
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseHealthChecks("/health");
+
+app.MapControllers();
+app.MapFallbackToFile("/index.html");
+app.Run();
 
 
 
